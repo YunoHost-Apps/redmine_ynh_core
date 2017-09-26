5f6ca27c4121918eb9eb9c05591bd0781e3a1c32
Maniack Crudelis <maniackc_dev@crudelis.fr>
2017-09-26 02:21:38 +0200

    Init - Install broken...

---
+++ conf/puma.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env puma
+
+application_path = '__FINALPATH__'
+directory application_path
+environment 'production'
+daemonize true
+pidfile "#{application_path}/tmp/pids/puma.pid"
+state_path "#{application_path}/tmp/pids/puma.state"
+stdout_redirect "#{application_path}/log/puma.stdout.log", "#{application_path}/log/puma.stderr.log"
+bind "unix://#{application_path}/tmp/sockets/redmine.sock"
